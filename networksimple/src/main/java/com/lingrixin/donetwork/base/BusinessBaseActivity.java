4690c74527752d5d4d28d67ec2e009e8998c02e9
rixin <lingrixin@live.cn>
2017-04-05 07:03:17 +0800

    添加 部分布局

---
+++ networksimple/src/main/java/com/lingrixin/donetwork/base/BusinessBaseActivity.java
@@ -0,0 +1,17 @@
+package com.lingrixin.donetwork.base;
+
+/**
+ * Created by songy on 2017/4/3.
+ */
+
+public class BusinessBaseActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
---
+++ networksimple/src/main/java/com/lingrixin/donetwork/business/urlhttpconnection/UrlHttpConnectionActivity.java
@@ -0,0 +1,19 @@
+package com.lingrixin.donetwork.business.urlhttpconnection;
+
+import com.lingrixin.donetwork.base.BaseActivity;
+
+/**
+ * Created by songy on 2017/4/3.
+ */
+
+public class UrlHttpConnectionActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
